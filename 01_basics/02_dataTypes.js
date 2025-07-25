513745d2dec122d09ca42b52eb01862355e60794
Devaraj V <devarajv.tech@gmail.com>
2025-07-25 11:41:54 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // it means- treat all js code as newer version
+
+let name = "Devaraj" // string datatype
+let age = 21 // number
+let isLoggedIn = false
+
+
+/*
+Primitive Dtatypes
+1. number
+2. bigint
+3. string => ""
+4. boolean => true/ false
+5. null => standalone value
+6. undefined => means right now a value is not assigned
+7. symbol => to find the uniqueness of different components
+*/
+
+// objects
+
+console.log(typeof"Dev");   // o/p => string
+console.log(typeof undefined);  // o/p => undefined
+console.log(typeof null);   // o/p => object
