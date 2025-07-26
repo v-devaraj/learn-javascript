38bb56b039a8f7f74a7954137844b470b2456ba0
Devaraj V <devarajv.tech@gmail.com>
2025-07-26 18:59:32 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,5 @@
+console.log(1 > 2)
+console.log(null > 0)   /*kindly note that this type of comparing null or undefined with a number can be problematic. Hence avaoid these kinds of comparisons as far as possible.*/
+
+// strict check (===): compares not just the values but also the datatypes
+
