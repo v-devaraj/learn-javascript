83ded0f1a718027ae849bc4c73bbf014a45f2c54
Devaraj V <devarajv.tech@gmail.com>
2025-07-29 12:20:15 +0000

    strings-second-update

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -12,9 +12,28 @@ console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
 */
 
 // alternate way of initializing the string:
-const gameName = new String('Devhc')
+const gameName = new String('Dev-hc')
 console.log(gameName[0])
-console.log(gameName.__proto__)
+console.log(gameName.__proto__) // Ans is {}
 console.log(gameName.length)
 console.log(gameName.toUpperCase()) /*this operation does not change our original string as these strings are stored in stack memory.*/
 
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('v'))
+
+// const newString = gameName.substring(0, 4)
+// note that the end index is excluded.
+
+// console.log(newString)
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString)
+
+/* trim and replace method */
+const newStringOne = '   Dev   '
+console.log(newStringOne)
+console.log(newStringOne.trim())
+console.log(newStringOne.replace('De', 'Ar'))
+console.log(newStringOne.replace('De', 'Ar').trim())
+console.log(gameName.split('-'))    // using split method in strings
+console.log(gameName.split('-'))
